2b2954d1fbf2f0271e04b61cc1b662410033e85f
Jacob Rein <jakerein@gmail.com>
2021-10-19 06:23:56 -0400

    asdf

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ buildscript {
     dependencies {
         classpath(BuildPlugins.androidGradlePlugin)
         //classpath(BuildPlugins.kotlinGradlePlugin)
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${BuildPlugins.Versions.kotlinVersion}")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
         classpath("com.github.dcendents:android-maven-gradle-plugin:2.1")
 
         // NOTE: Do not place your application dependencies here; they belong
